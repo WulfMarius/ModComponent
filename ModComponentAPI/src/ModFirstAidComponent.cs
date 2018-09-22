fb8037156a21bfef792c9b092b3cac6a80f662d9
nbakulev <nikolay.bakulev@gmail.com>
2018-09-23 00:41:11 +0300

    removed unued ModFirstAidcomponent attributes

--- ModComponentAPI/src/ModFirstAidComponent.cs
+++ ModComponentAPI/src/ModFirstAidComponent.cs
@@ -4,27 +4,41 @@ namespace ModComponentAPI
 {
     public class ModFirstAidComponent : ModComponent
     {
-        [Header ("First Aid")]
-        [Tooltip("Immediately HP retore")]
-        public float HPIncrease;
+        [Header("First Aid")]
+
         [Tooltip("Does affect like Antibiotics?")]
         public bool ProvidesAntibiotics;
+
         [Tooltip("Does affect like Painkiller?")]
         public bool KillsPain;
+
         [Tooltip("Does affect like Bandage?")]
         public bool AppliesBandage;
-        [Tooltip("Does affect like Bandage?")]
-        public bool AppliesSutures;
-        [Tooltip("Does affect like Bandage?")]
-        public bool StabalizesSprains;
+
         [Tooltip("Does it clean wounds?")]
         public bool CleansWounds;
+
         [Tooltip("Time to use in-game seconds")]
         public float TimeToUseSeconds;
+
         [Tooltip("Use audio")]
         public string UseAudio;
+
         [Tooltip("unis per use")]
         public int UnitsPerUse;
+
+        [HideInInspector]
+        [Tooltip("Immediately HP retore. Unused")]
+        public float HPIncrease;
+
+        [HideInInspector]
+        [Tooltip("Unesed at this moment")]
+        public bool AppliesSutures;
+
+        [HideInInspector]
+        [Tooltip("Does affect like ... i don't like what. Unused ")]
+        public bool StabalizesSprains;
+        
     }
 
 }
