9061a4142c3938c8067597f474e3510e56476f8b
nbakulev <30606360+nbakulev@users.noreply.github.com>
2018-09-22 23:31:18 +0300

    Add files via upload

---
+++ Example/VisualStudio/src/Implementation.cs
@@ -0,0 +1,25 @@
+﻿using ModComponentAPI;
+using ModComponentMapper;
+
+namespace RubberDuck
+{
+    public class Implementation
+    {
+        public ModComponent ModComponent;
+
+        public void OnEquipped()
+        {
+            ShowButtonPopups();
+        }
+
+        public void OnPrimaryAction()
+        {
+            ModUtils.PlayAudio("Play_UseRubberDuck");
+        }
+
+        private static void ShowButtonPopups()
+        {
+            EquipItemPopupUtils.ShowItemPopups(Localization.Get("GAMEPLAY_Squeeze"), string.Empty, false, false, false, true);
+        }
+    }
+}
