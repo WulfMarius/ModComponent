3e1226c772540c1d6714b07ef24623e5d95169ca
nbakulev <nikolay.bakulev@gmail.com>
2018-10-02 02:26:02 +0300

    added EvolveComponent (Evloved Items )

---
+++ ModComponentAPI/src/ModEvolveComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ModComponentAPI
+{
+    public class ModEvolveComponent : MonoBehaviour
+    {
+        [Header("Evolve Item")]
+
+        [Tooltip("Name of item to become. If This is a new item")]
+        public GameObject GearItemToBecome;
+
+        [Tooltip("In game gear item name to become. Start with GEAR_")]
+        public string GearItemName;
+
+        [Tooltip("Start evolve percent")]
+        [Range(0f, 100f)]
+        public int StartEvolvePercent;
+
+        [Tooltip("Days to evolve to new item")]
+        public float TimeToEvolveGameDays;
+
+        [Tooltip("Evolve only indoors")]
+        public bool RequireIndoors;
+
+    }
+}
